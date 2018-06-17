e6a567a55032917a34db48c94b686a83c54898c7
Afro <aifric.nolan.gold@gmail.com>
2018-06-17 14:29:50 +0100

    Editied links

--- mesopotamia/meso.py
+++ mesopotamia/meso.py
@@ -15,9 +15,10 @@ dot.node("J", "Seleucid Empire")
 dot.node("K", "Parthian Empire")
 dot.node("L", "Roman Empire")
 dot.node("N", "Achaeminid Empire")
+dot.node("O", "Achaeminid Empire")
 dot.node("M", "Elam (2700 BCE)")
 
-dot.edges(["AB", "BC", "BD", "CE", "DF", "EG", "GH", "HI","IJ", "JK", "KL", "FH", "MN"])
+dot.edges(["AB", "BC", "BD", "CE", "DF", "EG", "GH", "HI","IJ", "JK", "KL", "FN", "MO"])
 
 dot.render('test-output/meso.gv', view=True)  
 'test-output/mesopotamia.gv.pdf'
